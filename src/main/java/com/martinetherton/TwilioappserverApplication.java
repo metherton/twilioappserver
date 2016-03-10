d29fe1550d48c6955ac9b4d06db7f62af6276173
metherton <martinetherton63@gmail.com>
2016-03-10 21:31:12 +0100

    first commit

---
+++ src/main/java/com/martinetherton/TwilioappserverApplication.java
@@ -0,0 +1,12 @@
+package com.martinetherton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwilioappserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwilioappserverApplication.class, args);
+	}
+}
